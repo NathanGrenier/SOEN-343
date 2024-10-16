0f6959dda2ec591f2f55121423476f8aec86168d
NathanGrenier <nathangrenier01@gmail.com>
2024-10-15 20:04:56 -0400

    generated vite app

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+})
+
