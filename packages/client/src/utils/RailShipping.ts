69763192683dc52b86c7478313426849ad0bdb33
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2024-11-12 14:59:48 -0500

    merge

--- packages/client/src/utils/RailShipping.ts
+++ packages/client/src/utils/RailShipping.ts
@@ -1,7 +1,13 @@
+//RailShipping.ts
+
 import { ShippingMethodStrategy } from "./ShippingMethodStrategy";
 
 export class RailShipping implements ShippingMethodStrategy {
+    private shippingFee = 35;
   getName(): string {
     return "Rail Shipping";
+  }
+  getFee(): number{
+    return this.shippingFee;
   }
 }
