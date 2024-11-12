1d790a895db7935314e0fa5064fa426118dce996
TheMasterProgrammer <ryanrebbas2001@gmail.com>
2024-11-12 15:10:40 -0500

    merge conflict resolving again

--- packages/client/src/utils/ShippingCostCalculator.ts
+++ packages/client/src/utils/ShippingCostCalculator.ts
@@ -11,7 +11,9 @@ export class ShippingCostCalculator {
     this.strategy = strategy;
   }
 
-  calculate(weight: number, isExpress: boolean, shippingFee: number): number {
-    return this.strategy(weight, isExpress, shippingFee);
+  // Allow calling with or without shippingFee
+  calculate(weight: number, isExpress: boolean, shippingFee?: number): number {
+    // If shippingFee is undefined, pass 0 to the strategy
+    return this.strategy(weight, isExpress, shippingFee ?? 0);
   }
 }
